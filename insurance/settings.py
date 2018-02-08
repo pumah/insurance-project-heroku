baa2f72427a89e7ce1649fa99ad1e9749cd1ea01
pumah <Mahendra_131@rediffmail.com>
2018-02-08 19:35:54 +0530

    Update settings.py

--- insurance/settings.py
+++ insurance/settings.py
@@ -87,9 +87,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'sql9219993',
         'USER': 'sql9219993',
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': config('DATABASE_HOST'),
-        'PORT': config('DATABASE_PORT'),
+        'PASSWORD': 'ahckdGq9CA',
+        'HOST': 'sql9.freesqldatabase.com',
+        'PORT': '3306',
     }
 }
 
