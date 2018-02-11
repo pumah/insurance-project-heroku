031eae0a324a3a260407eaa425bc741116434566
pumah <Mahendra_131@rediffmail.com>
2018-02-11 12:48:22 +0530

    Update views.py

--- riskapp/views.py
+++ riskapp/views.py
@@ -5,38 +5,13 @@ from rest_framework import generics
 from .serializers import riskSerializer
 from .models import risk_type, risk_field
 from django.views.generic.edit import CreateView
-'''
-from rest_framework.response import Response
 
-class RiskListView(generics.ListCreateAPIView):
-
-    def get(self, request, format = None):
-        risks = risk_type.objects.all()
-        serializer = riskfldSerializer(risks, many = True)
-        return Response(serializer.data)
-
-    def post(self, request, format = None):
-        serializer = riskfldSerializer(data = request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status = status.HTTP_201_CREATED)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
-'''
 class CreateRiskView(generics.ListCreateAPIView):
-    """This class defines the create behavior of our rest api."""
     queryset = risk_type.objects.all()
     serializer_class = riskSerializer
 
     def perform_create(self, serializer):
-        """Save the post data when creating a new risk."""
         serializer.save()
-
-#class DetailsView(generics.RetrieveUpdateDestroyAPIView):
- #   """This class handles the http GET, PUT and DELETE requests."""
-
- #   queryset = risk_type.objects.all()
- #   serializer_class = riskSerializer
 
 class ReadView(generics.ListAPIView):
     queryset = risk_type.objects.all()
@@ -59,7 +34,3 @@ class RfldCreate(CreateView):
 
     def dispatch(self, *args, **kwargs):
             return super(RfldCreate, self).dispatch(*args, **kwargs)
-
-
-
-
